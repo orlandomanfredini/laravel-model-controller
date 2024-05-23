c13e74a6403c29e1899164be5d35b1e8cf42696e
orlando manfredini <orlando.manfre1@gmail.com>
2024-05-23 15:30:01 +0200

    ADD: database

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header class="bg-danger p-3">
+    <div class="container">
+        <div class="row">
+            <ul class="list-unstyled p-0 m-0 d-flex align-items-center">
+                <li><a href="{{route('movie')}}">Movie</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
+
